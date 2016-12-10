b909da49850eb47d985d554199a36b091a87ec9e
mrmaninew <manikanta@live.in>
2016-12-11 00:02:37 +0530

    installed new packages and port configs

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express'),
     path = require('path'),
     hbs = require('hbs'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
     app = express();
 
+
+// Port 
+app.set('PORT', process.env.PORT || 3000);
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -25,6 +34,6 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("server running on 3000");
+app.listen(app.get('PORT'), () => {
+    console.log(`server running on ${app.get('PORT')}`);
 });
